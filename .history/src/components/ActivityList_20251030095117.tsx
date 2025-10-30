692c58e7c467a81914fbc77b636f5880b33e493a
Samuel-Leal96 <saam_lv96@hotmail.com>
2025-10-30 10:48:24 -0600

    Mostrando el componente para las actividades guardadas

--- .history/src/components/ActivityList_20251030095117.tsx
+++ .history/src/components/ActivityList_20251030095117.tsx
@@ -1,29 +1,25 @@
 import React, { useMemo } from 'react'
 import type { Activity } from '../types'
-import type { Category } from '../types'
-import { categories } from '../data/categories'
+import type { Category } from '../types' 
 
 type ActivityListProps = {
     activities: Activity[]
 }
 
-export default function ActivityList({ activities }: ActivityListProps) {
+export default function ActivityList( {activities} : ActivityListProps) {
 
-    const categoryName = useMemo(() =>
-        (category: Activity['category']) => categories.map( cat => cat.id === category ? cat.name : '')
-            ,[activities])
+    const categoryName = useMemo( () => (category: Activity['category']) => console.log(category) , [activities])
 
     return (
         <>
             <h2 className='text-4xl font-bold text-slate-600 text-center'> Comida y actividades</h2>
 
-            {activities.map(activity => (
+            { activities.map( activity => (
                 <div key={activity.id} className='px-5 py-10 bg-white mt-5 flex justify-between'>
 
                     <div className="space-y-2 relative">
-                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
-                            ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
-                            {categoryName(+activity.category)}
+                        <p>
+                            {activity.category}
                         </p>
                         <p className='text-2xl font-bold pt-5'>
                             {activity.name}
@@ -37,8 +33,8 @@ export default function ActivityList({ activities }: ActivityListProps) {
                     <div className=""></div>
 
                 </div>
-            ))}
-
+            ) ) }
+        
         </>
     )
 }
--- .history/src/components/ActivityList_20251030095550.tsx
+++ .history/src/components/ActivityList_20251030095550.tsx
@@ -21,8 +21,7 @@ export default function ActivityList({ activities }: ActivityListProps) {
                 <div key={activity.id} className='px-5 py-10 bg-white mt-5 flex justify-between'>
 
                     <div className="space-y-2 relative">
-                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
-                            ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                        <p>
                             {categoryName(+activity.category)}
                         </p>
                         <p className='text-2xl font-bold pt-5'>
